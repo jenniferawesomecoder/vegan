6b898c882b5c17ae0ad88f2d9f441b58f870e9ed
jenniferawesomecoder <dogdoglovernumerouno1985@gmail.com>
2020-07-06 23:54:18 +0530

    carousel

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Carousel, { Modal, ModalGateway } from 'react-images';
 
 const src = [
   "https://verduraphx.com/wp-content/uploads/2019/02/chickpea-chana.jpg",
   "https://verduraphx.com/wp-content/uploads/2019/02/goth-waffles.jpg",
   "https://verduraphx.com/wp-content/uploads/2019/02/poboy.jpg"
 ]
+const images = src.map(f => ({source: f}))
 
 const Gallery = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [lightboxIsOpen, setLightboxIsOpen] = useState(false);
+
+  const toggleLightbox = (j) => {
+    setLightboxIsOpen(!lightboxIsOpen);
+    setSelectedIndex(j);
+  };
+
   return (
     <div className="main-content">
-      {src.map(f => (
-        <div key={f} className="image-grid">
+      {src.map((f, j) => (
+        <div key={f} className="image-grid" onClick={() =>  toggleLightbox(j)}>
           <img className="image" src={f} alt={f} />
         </div>
       ))}
+
+      <ModalGateway>
+        {lightboxIsOpen ? (
+          <Modal onClose={toggleLightbox}>
+            <Carousel
+              currentIndex={selectedIndex}
+              views={images}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   )
 }
