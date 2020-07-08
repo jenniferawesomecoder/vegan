f69abfe69610bfd4cbab553f51f58a981cf6b9de
jenniferawesomecoder <dogdoglovernumerouno1985@gmail.com>
2020-07-08 23:48:47 +0530

    Updates

---
+++ precache-manifest.98da4e0b7d30a7fc96b08bb332deb521.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82ed699bc49e63d398ee5616ae7571b",
+    "url": "/vegan/index.html"
+  },
+  {
+    "revision": "b7a602d02620d4c316b9",
+    "url": "/vegan/static/css/main.fdc4c364.chunk.css"
+  },
+  {
+    "revision": "5b8e071c0c1f30410ca2",
+    "url": "/vegan/static/js/2.c83cff73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vegan/static/js/2.c83cff73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a602d02620d4c316b9",
+    "url": "/vegan/static/js/main.8174c760.chunk.js"
+  },
+  {
+    "revision": "8c3fad27466877eb1301",
+    "url": "/vegan/static/js/runtime-main.d9f1a1d7.js"
+  },
+  {
+    "revision": "63291ae60f0a64d13b8981755b6680c5",
+    "url": "/vegan/static/media/logo.63291ae6.png"
+  }
+]);
